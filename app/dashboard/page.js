ddc573d18f041f734303f2ff195aa065ec9e1524
Day Blassingame <dayblassingame@gmail.com>
2024-07-11 21:07:42 -0400

    search component added

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,16 +1,15 @@
 import React from "react";
-import { FaUmbrella } from "react-icons/fa";
+import Search from "./search";
 import styles from '../styles/components/dashboard.module.scss';
 import icon from '../styles/components/icon.module.scss';
 
+
 export default function comingSoon(){
-    
+
     return(
         <main>
-            <div  className={styles.container}>
-                <div className={styles.content}>
-                    <h1> Coming Soon </h1>
-                </div>
+            <div className={styles.container}>
+            <Search/>
             </div>
         </main>
     )
---
+++ app/dashboard/search.js
@@ -0,0 +1,28 @@
+"use client";
+
+import React, { useEffect, useRef } from "react";
+
+export default function search(){
+    const searchForm = useRef(null);
+
+    useEffect(()=>{
+        searchForm.current.addEventListener('onKeyPress', keyPress);
+    },[])
+
+    const keyPress=(event)=>{
+        if(event.key==='enter'){
+            searchForm.submit();
+        }
+    }
+
+    const handleSubmit=(e)=>{
+    }
+
+    return(
+        <div className=''>
+            <form ref={searchForm} onSubmit={handleSubmit}>
+                <input id='searchInput' name='search' placeholder="Search for cities"/>
+            </form>
+        </div>
+    )
+        }
