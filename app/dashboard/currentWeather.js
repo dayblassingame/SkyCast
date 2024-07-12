8668e0bc065fa1bbc38c7625ae3ee0640229679a
Day Blassingame <dayblassingame@gmail.com>
2024-07-11 21:19:18 -0400

    current weather wireframe

---
+++ app/dashboard/currentWeather.js
@@ -0,0 +1,17 @@
+import react from 'react';
+
+export default function currentWeather(){
+
+    return(
+        <div>
+            <div>
+                <h2>City Name</h2>
+                <p>Change of rain: %</p>
+                <h1>Temperature</h1>
+            </div>
+            <div>
+                <img name='weather status'/>
+            </div>
+        </div>
+    )
+}
--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Search from "./search";
+import CurrentWeather from "./currentWeather";
 import styles from '../styles/components/dashboard.module.scss';
 import icon from '../styles/components/icon.module.scss';
 
@@ -9,7 +10,8 @@ export default function comingSoon(){
     return(
         <main>
             <div className={styles.container}>
-            <Search/>
+                <Search/>
+                <CurrentWeather/>
             </div>
         </main>
     )
